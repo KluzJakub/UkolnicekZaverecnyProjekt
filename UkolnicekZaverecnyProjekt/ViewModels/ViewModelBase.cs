018917d5ae765236a9c62f8c0df9ca2bdd4da129
Jakub Klůz <kluz.kuba@gmail.com>
2024-06-12 11:36:16 +0200

    Initial commit

---
+++ UkolnicekZaverecnyProjekt/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace UkolnicekZaverecnyProjekt.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ UkolnicekZaverecnyProjekt/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace UkolnicekZaverecnyProjekt.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
