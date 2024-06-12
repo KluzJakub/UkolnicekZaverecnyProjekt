9b544a54f71067190bda5a876e76c2fe09a2bbbf
Honza Běhunčík <112960672+honzabehuncik@users.noreply.github.com>
2024-06-12 12:10:14 +0200

    Added model and simple db

---
+++ UkolnicekZaverecnyProjekt/Database/Database.cs
@@ -0,0 +1,16 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Todo.Models;
+ 
+namespace UkolnicekZaverecnyProjekt.Database;
+
+public class Database
+{
+    public IEnumerable<TodoItem> GetItems() => new[]
+    {
+        new TodoItem { Name = "Walk the dog" },
+        new TodoItem { Name = "Buy some milk" },
+        new TodoItem { Name = "Learn Avalonia", IsChecked = true },
+    };
+}
---
+++ UkolnicekZaverecnyProjekt/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Todo.Models
+{
+    public class TodoItem
+    {
+        public string Name { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
