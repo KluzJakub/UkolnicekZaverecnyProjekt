31127d9e9c90589e664eb9bdfdd245b7f27011fa
Honza Běhunčík <112960672+honzabehuncik@users.noreply.github.com>
2024-06-12 11:58:25 +0200

    First ui/ux step

--- UkolnicekZaverecnyProjekt/Views/MainWindow.axaml.cs
+++ UkolnicekZaverecnyProjekt/Views/MainWindow.axaml.cs
@@ -1,11 +1,14 @@
+using System.Net.Mime;
 using Avalonia.Controls;
 
 namespace UkolnicekZaverecnyProjekt.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+        public MainWindow()
+        {
+            InitializeComponent();
+            tasks.ItemsSource = new string[]
+                {"První úkol", "Druhý úkol", "Třetí úkol" };
+        }
 }
